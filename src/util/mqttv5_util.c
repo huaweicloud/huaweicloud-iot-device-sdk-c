3ded6b80b8437d78848829cd134e854f91bb5859
longshijing <1417449807@qq.com>
2022-10-11 15:53:55 +0800

    增加MQTT5.0â

--- src/util/mqttv5_util.c
+++ src/util/mqttv5_util.c
@@ -4,17 +4,17 @@
 
 #if defined(MQTTV5)
 //Linked list deletion
-int listFree(MQTTV5_USER_PRO *mqtt_data){
-    if(mqtt_data == NULL){
+int listFree(MQTTV5_USER_PRO *mqtt_data) {
+    if(mqtt_data == NULL) {
         return 0;
     }
     MQTTV5_USER_PRO *propertiesA = mqtt_data;
     MQTTV5_USER_PRO *propertiesB = mqtt_data->nex;
 
-    while(propertiesA != NULL){
+    while(propertiesA != NULL) {
         free(propertiesA);
         propertiesA = propertiesB;
-        if(propertiesA != NULL){
+        if(propertiesA != NULL) {
             propertiesB = propertiesA->nex;
         }        
     }
